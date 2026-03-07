33caa2c7107baed0ed0322381f236c4662692bee
sailendraprasath <sailendraprasath05@gmail.com>
2026-03-07 21:45:52 +0530

    well

--- Backend/config/wsgi.py
+++ Backend/config/wsgi.py
@@ -2,7 +2,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend.settings")
+
 
 application = get_wsgi_application()
 
--- Backend/manage.py
+++ Backend/manage.py
@@ -5,7 +5,8 @@ import sys
 
 def main() -> None:
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
